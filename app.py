e1a6b61e5fbbd2531bcf44117783f294fbd86885
Petya <vegvaripeter21@gmail.com>
2025-08-05 17:10:59 +0200

    remove unused columns

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_json("partners.json", orient="records")
+testing = true
 
-st.title("JSON Table Explorer")
-st.dataframe(df)              # interactive table
+df = pd.read_json("partners.json", orient="records")
+if(testing):
+    df = df.head(10)
+    
+df = df.drop(['status', 'sumTopicsCount', 'departmentTopicsCount', 'isPremium'], axis=1)
+df.to_json("columnsRemoved.json", orient="records")
+
+_ = st.title("BME AUT Internship partners")
+
+for col in df.select_dtypes(include=['object']).columns:
+    search = st.text_input(f"Search {col}")
+    if search:
+        df = df[df[col].str.contains(search, case=False, na=False)]
+
+_ = st.dataframe(df)
